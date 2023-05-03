6954f664f16f2ea34769accde289c9b866bb009e
Dewa Aditya <dewadityasanjaya@gmail.com>
2023-05-03 15:05:09 +0700

    fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from './robots';
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.cypress.io/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users })
+            );
     }
 
     onSearchChange = (event) => {
@@ -20,14 +26,22 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
         })
-        return (
-            <div className="tc">
-                <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
-            </div>
+        if (this.state.robots.length === 0) {
+            return (
+                <div className="tc">
+                    <h1>LOADING</h1>
+                </div>
+            );
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots} />
+                </div>
 
-        );
+            );
+        }
     }
 }
 
