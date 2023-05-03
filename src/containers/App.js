89a6576d091d4f99a0c366a3094b1c67ecbbaf38
Dewa Aditya <dewadityasanjaya@gmail.com>
2023-05-03 15:34:22 +0700

    cleaning code

--- src/containers/App.js
+++ src/containers/App.js
@@ -24,19 +24,19 @@ class App extends Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+        const { robots, searchField } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase())
         })
-        if (this.state.robots.length === 0) {
-            return (
+        return (!robots.length) ?
+            (
                 <div className="tc">
                     <h1>LOADING</h1>
                 </div>
-            );
-        } else {
-            return (
+            ) :
+            (
                 <div className="tc">
-                    <h1 className="f1">RoboFriends</h1>
+                    <h1 className="f1">ROBOFRIENDS</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
                         <CardList robots={filteredRobots} />
@@ -44,7 +44,6 @@ class App extends Component {
                 </div>
 
             );
-        }
     }
 }
 
